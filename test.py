f0d05ec16d22bff0f66488689e689bc78401da7c
AlfaBettaGamma <surmenko_konstantin@mail.ru>
2019-06-15 10:49:16 +0300

    final

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
-def HowManyTimes(s, s_generic)
+def HowManyTimes(s,s_generic):
+    st = [] 
+    count = 0
+    for i in s_generic:
+        st.append(i)
+    a = combi(st, len(s));
+    for el in a:
+        if el == s:
+            count = count + 1  
+    return count
+
+def combi(el, size):
+    if len(el) < size size == 1:
+        return el
+ 
+    result = []
+    for i, item in enumerate(el):
+        for j in combi(el[i + 1:], size - 1):
+            result.append(item + j)
+    return result
